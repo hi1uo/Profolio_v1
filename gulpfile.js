bbd675fa4bb44551471378e6a1134f1163672d3c
jerry <Jerry@guojings-MacBook-Pro.local>
2016-04-10 20:05:11 -0500

    First complete version

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
+var minifyCSS   = require('gulp-minify-css');
 var reload      = browserSync.reload;
+var imagemin    = require('gulp-imagemin');
+var imageResize = require('gulp-image-resize');
+var rename      = require('gulp-rename');
+var responsive  = require('gulp-responsive-images');
+
+gulp.task('minicss',function(){
+  gulp.src('style/*.css')
+  .pipe(minifyCSS())
+  .pipe(gulp.dest('./style/'))
+})
+
 gulp.task('browserSync', function() {
     browserSync.init({
         server: {
@@ -9,3 +21,43 @@ gulp.task('browserSync', function() {
     });
     gulp.watch("./style/*.css").on('change',reload);
 });
+
+gulp.task('imagemin', function(){
+ // find the image sources
+  gulp.src('imgsrc/*.*')
+ //minify image
+  .pipe(imagemin({
+    progressive:true
+  }))
+  //save image to different folder
+  .pipe(gulp.dest('img/'))
+});
+
+
+//image resize, but not good at modify multiple with rename
+gulp.task('imgresize', function(){
+  gulp.src('imgsrc/*.*')
+  //small+rename
+    .pipe(imageResize({ width: 640}))
+    .pipe(rename(function(path){path.basename += "-small";}))
+    .pipe(gulp.dest('img/'))
+});
+
+gulp.task('responsive', function(){
+  gulp.src('imgsrc/*.*')
+    .pipe(responsive({
+      'tree_icon.*':[
+        {
+          width:320,
+          suffix: '-sm'
+        }
+        ,{
+        width: 640,
+        suffix: '-md'
+      },{
+        width: 1200,
+        suffix: '-lg'
+      }]
+    }))
+    .pipe(gulp.dest('img/'));
+})
